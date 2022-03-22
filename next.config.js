76a674e3508f4883262711d0183525856638b93a
SudeepMI <sudeepm0599@gmail.com>
2022-03-22 18:20:06 +0545

    production API

--- next.config.js
+++ next.config.js
@@ -6,6 +6,6 @@ module.exports = {
     publicRuntimeConfig: {
         apiUrl: process.env.NODE_ENV === 'development'
             ? 'http://localhost:3000/api' // development api
-            : 'http://localhost:3000/api' // production api
+            : 'https://notebook-eta.vercel.app' // production api
     }
 }
